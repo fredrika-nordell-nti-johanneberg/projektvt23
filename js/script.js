b1026152ee28418d2507a795b3db735fba58952b
fredrika-nordell-nti-johanneberg <118253533+fredrika-nordell-nti-johanneberg@users.noreply.github.com>
2023-04-04 10:50:40 +0200

    last minute fuckeries

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Denna fil skall innehålla JavaScript (JS)
+const hamburger = document.querySelector("#hamburgermeny")
+const nav = document.querySelector("nav")
+
+function toggleMenu(){
+    nav.classList.toggle("active")
+}
